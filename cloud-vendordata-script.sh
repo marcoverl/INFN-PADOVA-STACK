1e60154d020a6e6277c0cd65908e8259f1a0dcc6
marcoverl <marco.verlato@pd.infn.it>
2021-11-05 18:36:14 +0100

    Update cloud-vendordata-script.sh

--- cloud-vendordata-script.sh
+++ cloud-vendordata-script.sh
@@ -3,6 +3,7 @@ export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 export instance_id="`cat /var/lib/cloud/data/instance-id`"
 export SSH_CONFIG=/etc/ssh/sshd_config
 export CIPHERS="Ciphers aes128-ctr,aes128-gcm@openssh.com,aes192-ctr,aes256-ctr,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com"
+export KEX="KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256,diffie-hellman-group14-sha1"
 
 logger "Vendor data: configure sshd"
 
@@ -27,7 +28,15 @@ case $LINUX_DISTRIBUTION in
               echo "${CIPHERS}" >> ${SSH_CONFIG}
               echo "" >> ${SSH_CONFIG}
               pidof sshd && service sshd restart
-          fi 
+          fi
+          if ! grep -q "${KEX}" ${SSH_CONFIG}
+          then
+              sed -i 's/^KexAlgorithms/#KexAlgorithms/' ${SSH_CONFIG}
+              echo "${KEK}" >> ${SSH_CONFIG}
+              echo "" >> ${SSH_CONFIG}
+              pidof sshd && service sshd restart
+          fi
+          
           logger "Vendor data injected on $LINUX_DISTRIBUTION host"
           ;;
      *)
